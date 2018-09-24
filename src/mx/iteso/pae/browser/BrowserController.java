51f0b4c786ef66fa839a3492456277d5b2bf9bfb
ElBuenBerna <bernardo.elizondo1@gmail.com>
2018-09-24 02:59:38 -0500

    Ya quedó la interfáz gráfica. Si introduces una dirección (por ejemplo "www.google.com") ya carga. Sólo va a faltar lo de los botones y el ChoiceBox, que lo que se me ocurría es hacerlo con un stack, pero a ver si sale algo más eficiente de ustedes. Cualquier duda me preguntan

--- src/mx/iteso/pae/browser/BrowserController.java
+++ src/mx/iteso/pae/browser/BrowserController.java
@@ -1,14 +1,64 @@
 package mx.iteso.pae.browser;
 
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
 import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.MenuButton;
+import javafx.scene.control.TextField;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+import javafx.concurrent.Worker.State;
+import javafx.event.ActionEvent;
 
-public class BrowserController {
+public class BrowserController implements Initializable{
     MenuButton menuButtonClose;
-
+    @FXML 
+    Button btnPrevious;
+    @FXML
+    Button btnNext;
+    @FXML
+    Button btnSearch;
+    @FXML
+    ChoiceBox<String> cbHistorial;
+    @FXML
+    TextField tfDirecciones;
+    @FXML
+    private WebView wbWebView;
+    
+    WebEngine engine;
     @FXML
     protected void close() {
         Platform.exit();
     }
+    
+    public void previous() {
+    	
+    }
+    
+    public void next() {
+    	
+    }
+    
+    public void search(ActionEvent event) {
+    	String direccion = tfDirecciones.getText();
+    	if(tfDirecciones.getText() != "") {
+    		engine.load("http://" + direccion);
+    	}    	
+    }
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		engine = wbWebView.getEngine();		
+	}
 }
--- src/mx/iteso/pae/browser/Main.java
+++ src/mx/iteso/pae/browser/Main.java
@@ -1,19 +1,24 @@
 package mx.iteso.pae.browser;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 import javafx.stage.Stage;
+import javafx.concurrent.Worker.State;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage stage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("browser.fxml"));
-        primaryStage.setTitle("Java Browser");
-        primaryStage.setScene(new Scene(root, 900, 600));
-        primaryStage.show();
+        stage.setTitle("Java Browser");
+        stage.setScene(new Scene(root, 800, 600));
+        stage.show();
     }
 
 
